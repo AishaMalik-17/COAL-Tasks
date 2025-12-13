7f15e8fc3f1c37b2067d209af1c19f9db76cca96
Ayesha <aishaaisha1713@gmail.com>
2025-12-13 00:36:25 -0800

    Add files via upload

---
+++ Calculations/Reversing a 2-digit number.asm
@@ -0,0 +1,31 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+mov ah,1
+int 21h
+mov bl,al
+
+mov ah,1
+int 21h
+mov cl,al
+
+mov dl,' '
+mov ah,2
+int 21h
+
+mov dl,cl
+mov ah,2
+int 21h
+
+mov dl,bl
+mov ah,2
+int 21h
+
+ret
+
+
+
+
